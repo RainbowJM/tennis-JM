870da6130cb311235b6d381e7fbe961520164aa4
Jevona Magdalena <jmagdalena@ictual.com>
2022-09-08 16:13:25 +0200

    player domain

---
+++ src/main/java/nl/hu/sd/tennis/domain/Player.java
@@ -0,0 +1,32 @@
+package nl.hu.sd.tennis.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue
+    @Setter
+    @Getter
+    private int id;
+    @Setter
+    @Getter
+    private String name;
+
+    @Setter
+    @Getter
+    private int score;
+
+    public Player() {
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
