31b56cfe0d6d01f2c0523764032f2819302e7221
Jevona Magdalena <jmagdalena@ictual.com>
2022-09-20 18:13:08 +0200

    Integration test fase 1 done

---
+++ src/test/resources/data-h2_player_before.sql
@@ -0,0 +1,12 @@
+SET SCHEMA tennis;
+
+INSERT INTO `player`(`id`,`name`, `score`)
+VALUES(10, "Ellen", 15);
+
+INSERT INTO `player`(`id`,`name`, `score`)
+VALUES (11, "Lauren", 30);
+
+INSERT INTO `player`(`id`,`name`, `score`)
+VALUES (12, "Javis", 40);
+
+
